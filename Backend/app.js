f08d990f4708ce8999933fccd8fc591635323aa1
utamrauniyar <utamrauniyar@outlook.com>
2022-10-12 21:28:15 +0545

    initial

---
+++ Backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require("./routes/book-routes");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose.connect(process.env.Mongo_URI, {useUnifiedTopology: true}).then(() => console.log("Connected To Database"))
+    .then(() => {
+        app.listen(5000);
+    }).catch((err) => console.log(err));
+
+
+
+
